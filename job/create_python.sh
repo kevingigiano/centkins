de5cdf8911f907abce74a82eb4b3e28081cf04e5
Kevin Gigiano <Kevin.Gigiano@braxtontech.com>
2021-11-19 11:26:16 -0700

    Added all code for creating custom CentOS7 Jenkins container and a pipeline for creating a Python 3.8.8 RPM

---
+++ job/create_python.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+WORKDIR=$(pwd)
+cd /tmp/
+wget https://www.python.org/ftp/python/3.8.8/Python-3.8.8.tgz
+tar xzf Python-3.8.8.tgz
+
+cd Python-3.8.8
+./configure --prefix=/opt/python38
+make -j 8
+make altinstall
+
+tar cvf python38.tar.gz -C /opt python38
+cp python38.tar.gz $WORKDIR
